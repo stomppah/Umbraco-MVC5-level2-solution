f8241020c4a1cd9ab2a51684e6aac4d5621fa299
Anthony Woodward <two.woods@gmail.com>
2016-07-20 10:23:36 +0100

    exercise 13

--- Umbraco.Course/Level2/Events/Level2ApplicationEventHandler.cs
+++ Umbraco.Course/Level2/Events/Level2ApplicationEventHandler.cs
@@ -11,8 +11,20 @@ namespace Umbraco.Course.Level2.Events
     {
         protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication,ApplicationContext applicationContext)
         {
-            
+            ContentService.Publishing += ContentService_Publishing;
         }
 
+        private void ContentService_Publishing(IPublishingStrategy sender, PublishEventArgs<IContent> e)
+        {
+            var badWords = new[] {"fudge", "shiza", "crap", "heck", "darn"};
+            foreach (var content in e.PublishedEntities)
+            {
+                if (!content.ContentType.Alias.Equals("statusUpdate")) continue;
+                var bodyText = content.GetValue<string>("bodyText").ToLower();
+                if (!badWords.Any(bodyText.Contains)) continue;
+                e.Cancel = true;
+                return;
+            }
+        }
     }
 }
