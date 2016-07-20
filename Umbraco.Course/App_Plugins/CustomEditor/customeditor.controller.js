d2c97115f8dfdfa5350d345e965293cd90e12cbe
Anthony Woodward <two.woods@gmail.com>
2016-07-20 13:21:39 +0100

    exercise 14

---
+++ Umbraco.Course/App_Plugins/CustomEditor/customeditor.controller.js
@@ -0,0 +1,11 @@
+﻿angular.module("umbraco").controller("My.CustomEditorController", function($scope, notificationservice) {
+    $scope.limitChars = function() {
+        var limit = parseInt($scope.model.config.limit);
+        if ($scope.model.value.length > limit) {
+            $scope.info = "You cannot write more than " + limit + " characters!";
+            $scope.model.value = $scope.model.value.substr(0, limit);
+        } else {
+            $scope.info = "You have " + (limit - $scope.model.value.length) + " characters left.";
+        }
+    };
+});
