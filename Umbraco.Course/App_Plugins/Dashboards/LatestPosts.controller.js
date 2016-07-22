067ac8cecf3bd5d045bf2b15fa122ceb9cadcbc6
Anthony Woodward <two.woods@gmail.com>
2016-07-22 14:20:49 +0100

    Exercise 16: Dashboard Controls

--- Umbraco.Course/App_Plugins/Dashboards/LatestPosts.controller.js
+++ Umbraco.Course/App_Plugins/Dashboards/LatestPosts.controller.js
@@ -1,5 +1,8 @@
 ﻿angular.module("umbraco")
-    .controller("level2.latestsPostsController", 
+    .controller("level2.latestsPostsController",
     function ($scope, $http, latestsPostsService) {
-   
-});
+        latestsPostsService.getAll()
+            .success(function(data) {
+                $scope.posts = data;
+            });
+    });
--- Umbraco.Course/Controllers/LatestPostsDashboardController.cs
+++ Umbraco.Course/Controllers/LatestPostsDashboardController.cs
@@ -9,8 +9,29 @@ namespace Umbraco.Course.Controllers
     {
         public IEnumerable<StatusUpdate> GetLatestsPost()
         {
-        
-            return null;
+            var type = Services.ContentTypeService.GetContentType("statusUpdate");
+
+            var statusUpdates = Services.ContentService.GetContentOfContentType(type.Id)
+                .OrderByDescending(x => x.CreateDate).Take(10);
+
+            var content = new List<StatusUpdate>();
+
+            foreach (var statusUpdate in statusUpdates)
+            {
+                var memberId = statusUpdate.GetValue<int>("memberId");
+                var memberName = memberId == 0
+                    ? "Guest"
+                    : Services.MemberService.GetById(memberId).Name;
+
+                content.Add(new StatusUpdate()
+                {
+                    Id = statusUpdate.Id,
+                    Text = statusUpdate.GetValue<string>("bodyText"),
+                    CreateDate = statusUpdate.CreateDate.ToString(),
+                    MemberName = memberName
+                });
+            }
+            return content;
         }
 
     }
